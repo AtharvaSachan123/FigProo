620414e54435a1304598afb85642c322392c25f1
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-04-30 00:17:38 +0530

    Chat implemented

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -15,7 +15,12 @@ const Cursor = ({color,x,y,message}:Props) => {
 
     <CursorSVG color={color} />
 
-    {/* {message} */}
+    {message &&(
+      <div className='absolute left-2 top-5 rounded-3xl px-4 py-2' style={{backgroundColor:color}}>
+        <p className='whitespace-nowrap text-sm leading-relaxed text-white'>{message}</p>
+        </div>
+
+    )}
     </div>
   )
 }
